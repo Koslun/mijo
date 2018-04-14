f1458ccf618b4b4fc596e5efd6cf22f56188c240
epoxy <palmqvist.anton@gmail.com>
2018-04-14 16:00:44 +0200

    feat(answerModel): added time property

--- server/models/mojiMessage/mojiMessageContent/answer.model.js
+++ server/models/mojiMessage/mojiMessageContent/answer.model.js
@@ -6,6 +6,7 @@ const Schema = mongoose.Schema;
  * This template is used for the answers to questions.
  */
 const AnswerSchema = new Schema({
+  timeAnswered: { type: Date, required: true },
   yesAnswer: { type: Boolean },
   textAnswerArray: { type: [String] },
   scaleAnswer: { type: Number },
