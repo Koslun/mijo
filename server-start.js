9253d694fa694f6c01cedb42462e9c446beb07eb
epoxy <palmqvist.anton@gmail.com>
2018-04-14 19:30:44 +0200

    fix(api): mlab password auth working

--- server-start.js
+++ server-start.js
@@ -2,6 +2,7 @@ process.env.NODE_ENV= process.env.NODE_ENV || 'development'
 process.env.SERVER_PORT= process.env.SERVER_PORT || '4040'
 process.env.JWT_SECRET='0a6b944d-d2fb-46fc-a85e-0295c986cd9f'
 process.env.MONGO_HOST='mongodb://ds119059.mlab.com:19059/mijo'
+process.env.MONGO_HOST='mongodb://mijo:hack3denmark@ds119059.mlab.com:19059/mijo'
 process.env.MONGO_PORT='19059'
 require('babel-register');
 require("babel-polyfill");
