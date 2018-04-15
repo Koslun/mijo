e8a858c9237c68c13881bb3e739070bbf8180e00
Koslun <adam.wamai@gmail.com>
2018-04-15 02:50:40 +0200

    design(server-start): add newline

--- server-start.js
+++ server-start.js
@@ -5,6 +5,7 @@ process.env.JWT_SECRET='0a6b944d-d2fb-46fc-a85e-0295c986cd9f';
 // process.env.MONGO_HOST='mongodb://ds119059.mlab.com:19059/mijo';
 // mlab db on epoxy account
 // process.env.MONGO_HOST='mongodb://mijo:hack3denmark@ds119059.mlab.com:19059/mijo';
+
 // MongoDB Atlas db on koslun account, PW: LJzW2SAHvOWW6vkR
 // TODO: remove hard-coded password
 process.env.MONGO_HOST='mongodb://admin_user:LJzW2SAHvOWW6vkR@mijo-shard-00-00-trk4r.mongodb.net:27017,mijo-shard-00-01-trk4r.mongodb.net:27017,mijo-shard-00-02-trk4r.mongodb.net:27017/test?ssl=true&replicaSet=Mijo-shard-0&authSource=admin';
